620e8e2ab29cc8d11264692b07c5f1b4e5f0b9b7
NirashaSewwandi <e18329@eng.pdn.ac.lk>
2024-03-23 16:59:46 +0530

    Add changes

--- TODO_APP/ui/src/utils.js
+++ TODO_APP/ui/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://ljbfj64jkgkjmlykabnvun7or40ewoug.lambda-url.eu-north-1.on.aws/test";
+export const API_URL = "https://ljbfj64jkgkjmlykabnvun7or40ewoug.lambda-url.eu-north-1.on.aws/task";
